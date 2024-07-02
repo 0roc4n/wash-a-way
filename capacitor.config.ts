8a3a2d2352fc412021604d98af17585dc571d17a
0roc4n <esparragojames@gmail.com>
2024-07-02 14:23:07 +0800

    initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'wash-a-way',
+  webDir: 'www'
+};
+
+export default config;
---
+++ src/app/tab2/tab2.page.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+import { AlertController } from '@ionic/angular';
+@Component({
+  selector: 'app-tab2',
+  templateUrl: 'tab2.page.html',
+  styleUrls: ['tab2.page.scss']
+})
+export class Tab2Page {
+
+  constructor(private alertController: AlertController) {}
+
+  async presentAlert() {
+    const alert = await this.alertController.create({
+      header: 'A Short Title Is Best',
+      subHeader: 'A Sub Header Is Optional',
+      message: 'A message should be a short, complete sentence.',
+      buttons: ['Action'],
+    });
+
+    await alert.present();
+  }
+
+}
